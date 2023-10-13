a127817676200b090241d57a8fbd669ffb21a416
no80dy <no8odyas@gmail.com>
2023-10-13 11:39:06 +0300

    Edit post model structure, add slug

--- nobodyas/base/models.py
+++ nobodyas/base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+from django.db import IntegrityError
 
 
 class Tag(models.Model):
@@ -21,9 +23,26 @@ class Post(models.Model):
     is_featured = models.BooleanField(default=False)
 
     tags = models.ManyToManyField(Tag, null=True)
+    slug = models.SlugField(null=True, blank=True, unique=True)
 
     def get_absolute_url(self):
-        return reverse("post", kwargs={"id": self.pk})
+        return reverse("post", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            if Post.objects.filter(slug=self.slug).exists():
+                self.slug = self.generate_unique_slug(self.slug)
+        super().save(*args, **kwargs)
+
+    def generate_unique_slug(self, slug):
+        base_slug = slugify(self.title)
+        count = 1
+        new_slug = base_slug
+        while Post.objects.filter(slug=new_slug).exists():
+            new_slug = f"{base_slug}-{count}"
+            count += 1
+        return new_slug
 
     def __str__(self) -> str:
         return self.title
